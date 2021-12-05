01d446685719ed93ce4ec7faa738735125bd8bae
Emily Chen <yenjungc@smu.edu>
2021-12-05 16:16:08 +0800

    feat: [SHAR-4] home page config

---
+++ pages/add-post.js
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Nav from '../components/Nav';
+import styles from '../styles/Home.module.css';
+
+export default function AddPost() {
+    const { t, i18n } = useTranslation();
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handlePost = async (e) => {
+        e.preventDefault();
+
+        // reset error and message
+        setError('');
+        setMessage('');
+
+        // fields check
+        if (!title || !content) return setError('All fields are required');
+
+        // post structure
+        let post = {
+            title,
+            content,
+            published: false,
+            createdAt: new Date().toISOString(),
+        };
+        // save the post
+        let response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify(post),
+        });
+
+        // get the data
+        let data = await response.json();
+
+        if (data.success) {
+            // reset the fields
+            setTitle('');
+            setContent('');
+            // set the message
+            return setMessage(data.message);
+        } else {
+            // set the error
+            return setError(data.message);
+        }
+    };
+
+    return (
+        <div>
+            <Nav/>
+            <div className={styles.container}>
+                <form onSubmit={handlePost} className={styles.form}>
+                    {error ? (
+                        <div className={styles.formItem}>
+                            <h3 className={styles.error}>{error}</h3>
+                        </div>
+                    ) : null}
+                    {message ? (
+                        <div className={styles.formItem}>
+                            <h3 className={styles.message}>{message}</h3>
+                        </div>
+                    ) : null}
+                    <div className={styles.formItem}>
+                        <label>{t('CONTACT.TITLE')}</label>
+                        <input
+                            type="text"
+                            name="title"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                            placeholder={t('CONTACT.PLACEHOLDER')}
+                        />
+                    </div>
+                    <div className={styles.formItem}>
+                        <label>{t('CONTACT.CONTENT')}</label>
+                        <textarea
+                            name="content"
+                            onChange={(e) => setContent(e.target.value)}
+                            value={content}
+                            placeholder={t('CONTACT.PLACEHOLDER')}
+                        />
+                    </div>
+                    <div className={styles.formItem}>
+                        <button className="bg-pink-500 hover:bg-pink-400 text-white font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded"
+                        type="submit">{t('CONTACT.SUBMIT')}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
---
+++ pages/api/posts.js
@@ -0,0 +1,120 @@
+const { connectToDatabase } = require('../../lib/mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+export default async function handler(req, res) {
+    // switch the methods
+    switch (req.method) {
+        case 'GET': {
+            return getPosts(req, res);
+        }
+
+        case 'POST': {
+            return addPost(req, res);
+        }
+
+        case 'PUT': {
+            return updatePost(req, res);
+        }
+
+        case 'DELETE': {
+            return deletePost(req, res);
+        }
+    }
+}
+
+async function getPosts(req,res){
+    try {
+        // connect to the database
+        let { db } = await connectToDatabase();
+        // fetch the posts
+        let posts = await db
+            .collection('posts')
+            .find({})
+            .sort({ published: -1 })
+            .toArray();
+        // return the posts
+        return res.json({
+            message: JSON.parse(JSON.stringify(posts)),
+            success: true,
+        });
+    } catch (error) {
+        // return the error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
+
+async function addPost(req, res) {
+    try {
+        // connect to the database
+        let { db } = await connectToDatabase();
+        // add the post
+        await db.collection('posts').insertOne(JSON.parse(req.body));
+        // return a message
+        return res.json({
+            message: 'Post added successfully',
+            success: true,
+        });
+    } catch (error) {
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
+
+async function updatePost(req, res) {
+    try {
+        // connect to the database
+        let { db } = await connectToDatabase();
+
+        // update the published status of the post
+        await db.collection('posts').updateOne(
+            {
+                _id: new ObjectId(req.body),
+            },
+            { $set: { published: true } }
+        );
+
+        // return a message
+        return res.json({
+            message: 'Post updated successfully',
+            success: true,
+        });
+    } catch (error) {
+
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
+
+async function deletePost(req, res) {
+    try {
+        // Connecting to the database
+        let { db } = await connectToDatabase();
+
+        // Deleting the post
+        await db.collection('posts').deleteOne({
+            _id: new ObjectId(req.body),
+        });
+
+        // returning a message
+        return res.json({
+            message: 'Post deleted successfully',
+            success: true,
+        });
+    } catch (error) {
+
+        // returning an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
