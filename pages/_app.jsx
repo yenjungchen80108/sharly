b0d7edee49551baa15f7294ed77cd79ce83a5e87
Emily Chen <yenjungc@smu.edu>
2022-03-08 19:47:35 +0800

    feat: [SHAR-7] repo func

---
+++ pages/_app.jsx
@@ -0,0 +1,16 @@
+import '../assets/base.css';
+import '../styles/globals.css'
+import { Layout } from '../components/Layout';
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'react-hot-toast';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Toaster />
+      </Layout>
+    </ThemeProvider>
+  );
+}
