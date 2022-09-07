4873480ab2c716d903ef00bbd423f8105ece6986
Emily Chen <yenjungc@smu.edu>
2022-09-07 19:02:25 +0800

    fix: [SHAR-14] render donate item on main page

--- components/Item/Item.jsx
+++ components/Item/Item.jsx
@@ -2,37 +2,21 @@ import clsx from 'clsx';
 import styles from './Item.module.css';
 
 const Item = (props) => {
+  const { img, itemName, partnerId } = props.item;
   return (
-    <div className="grid grid-cols-2 gap-3">
     <div className={styles.itemWrap}>
-      <img className={styles.imgWrap} src={'/png/book.png'} alt="default_img"/>
+      <img className={styles.imgWrap} src={img} alt="default_img"/>
       <div className={styles.contentWrap}>
-        <div className="text-gray-900 font-bold text-xl mb-2">Books</div>
+        <div className="text-gray-900 font-bold text-xl mb-2">{itemName}</div>
         <div className="flex items-center">
-          {/* <img className="w-10 h-10 rounded-full mr-4" src={'/png/donation.png'} alt="Avatar of Writer"/> */}
           <div className="text-sm">
-            <p className="text-gray-600 leading-none">康福智能發展中心</p>
+            <p className="text-gray-600 leading-none">{partnerId}</p>
           </div>
         </div>
         <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4
             border-b-4 border-green-700 hover:border-green-500 rounded">查看更多</button>
       </div>
     </div>
-    <div className={styles.itemWrap}>
-    <img className={styles.imgWrap} src={'/png/book.png'} alt="default_img"/>
-    <div className={styles.contentWrap}>
-      <div className="text-gray-900 font-bold text-xl mb-2">Books</div>
-      <div className="flex items-center">
-        {/* <img className="w-10 h-10 rounded-full mr-4" src={'/png/donation.png'} alt="Avatar of Writer"/> */}
-        <div className="text-sm">
-          <p className="text-gray-600 leading-none">康福智能發展中心</p>
-        </div>
-      </div>
-      <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4
-          border-b-4 border-green-700 hover:border-green-500 rounded">查看更多</button>
-    </div>
-  </div>
-  </div>
   );
 };
 
--- page-components/Donate/index.jsx
+++ page-components/Donate/index.jsx
@@ -2,19 +2,21 @@ import { Spacer, Wrapper } from '../../components/Layout';
 import { Item } from '../../components/Item';
 import Link from 'next/link';
 import { Button } from '../../components/Button';
+import { LoadingDots } from '../../components/LoadingDots';
+import { useRouter } from 'next/router';
+import { useDonateItems } from '../../lib/donateItem';
+import styles from './Donate.module.css';
 // import { Post as PostItem } from '../../components/Post';
 // import Commenter from './Commenter';
 // import CommentList from './CommentList';
-import styles from './Donate.module.css';
 // import { useCards } from '../../lib/card';
-import { useRouter } from 'next/router';
 
 export const Donate = ({ item }) => {
   const router = useRouter();
+  const { donateItemData, isLoading, isError } = useDonateItems();
   const { cardId } = router.query;
   // router.query.category = 'book';
   // router.push(router);
-  // console.log('router',router);
   // const { data } = useCards();
   // if (!data) return <div></div>;
   return (
@@ -36,7 +38,16 @@ export const Donate = ({ item }) => {
           </Button>
         </div>
       </Link>
-      <Item />
+      <div className={styles.box}>
+      {donateItemData ? donateItemData.donateItems.map((item, id) => (
+          <Link
+            key={item._id}
+            href={`/card/${item._id}`}
+            passHref
+          ><Item item={item} key={item._id}></Item>
+          </Link>
+        )) : <LoadingDots className={styles.loading} />}
+      </div>
       {/* <h3>Donate-test</h3> */}
       {/* <Commenter post={post} />
       <CommentList post={post} /> */}
